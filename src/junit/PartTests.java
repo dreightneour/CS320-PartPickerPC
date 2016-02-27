9a2d09a8046a6cf46fc90d12773ae4acc9ffaa9c
Robert Jones <rjones38@ycp.edu>
2016-02-27 17:07:36 -0500

    junit test for parts

---
+++ src/junit/PartTests.java
@@ -0,0 +1,38 @@
+package junit;
+
+import junit.framework.TestCase;
+import partPickerPC.Part;
+
+public class PartTests extends TestCase {
+
+	Part p;
+	String sp = "3.4 GHz";
+	String manufact = "Intel";
+	String series = "i7";
+	double price = 249.99;
+	boolean onSale = true;
+	double percent = 50;
+	protected void setUp() throws Exception{
+		p = new Part();
+	}
+	public void testAttributes() throws Exception{
+		p.updateAttributes(sp);
+		p.updateAttributes(manufact);
+		p.updateAttributes(series);
+		assertEquals(3, p.getAttr().size());
+	}
+	
+	public void testPrice() throws Exception{
+		p.setPrice(price);
+		assertEquals(price, p.getPrice());
+	}
+	public void testOnSale() throws Exception{
+		p.setSale(onSale);
+		assertEquals(true, p.getSale());
+	}
+	public void testMarkDown() throws Exception{
+		p.setSalePercentage(percent);
+		assertEquals(percent, p.getSalePercent());
+	}
+
+}
