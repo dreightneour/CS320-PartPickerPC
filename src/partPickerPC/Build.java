f690acc217fc2cbb3d9db0f5bc36332cddff7f3e
Drevon <dreid3@ycp.edu>
2016-02-27 19:41:04 -0500

    Added to the build class, also added another constructor to Part

--- src/partPickerPC/Build.java
+++ src/partPickerPC/Build.java
@@ -1,7 +1,23 @@
 package partPickerPC;
 
+import java.util.ArrayList;
+
 public class Build 
 {
+	private ArrayList<Part> theBuild;  // holds all the parts
+	
+	
+	public boolean checkCompatible(Part added)				// for minimal this just checks if there's more than
+	{													    // one type of part in a build
+		for (int i = 0; i < theBuild.size(); i++)
+		{
+			if (theBuild.get(i).getType() == added.getType())
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 	public void test(){
 		System.err.println("1");
 	}
