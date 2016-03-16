9cd81402f85ce7ea15675610e7ea82d8857e67f9
hwilling <hwilling@ycp.edu>
2016-03-16 19:15:38 -0400

    user

--- src/partPickerPC/User.java
+++ src/partPickerPC/User.java
@@ -7,7 +7,7 @@ public class User
 	private String password;
 	
 	//constructor
-	public void User(String name, String password){
+	public User(String name, String password){
 		this.name = name;
 		this.password = password;
 	}
