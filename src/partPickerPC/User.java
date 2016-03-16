ae586ed681b4c7c5f677e66540edf9c4b5552946
hwilling <hwilling@ycp.edu>
2016-03-16 18:40:00 -0400

    user class

--- src/partPickerPC/User.java
+++ src/partPickerPC/User.java
@@ -2,5 +2,21 @@ package partPickerPC;
 
 public class User 
 {
+	private char name;
+	private char password;
 	
+	public void User(name, password){
+		this.name = name;
+		this.password = password;
+	}
+	
+	public char getName(){
+		
+		return User.name;
+	}
+	
+	public char getPassword(){
+		
+		return User.password;
+	}
 }
