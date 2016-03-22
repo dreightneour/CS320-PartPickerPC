88c66e8616cb58b7dbe9c26d97a20aa9fe26bda7
Daniel Reightneour <dreightn@ycp.edu>
2016-03-22 11:13:00 -0400

    All Searches done

--- src/partPickerPC/Search.java
+++ src/partPickerPC/Search.java
@@ -18,6 +18,10 @@ public class Search{
     	cpu = getCPU("http://www.newegg.com/Product/Product.aspx?Item=N82E16819117369");
     	MotherboardPart mother;
     	mother = getMotherboard("http://www.newegg.com/Product/Product.aspx?Item=N82E16813130770");
+    	GpuPart gpu;// = getCPU();
+    	gpu = getGpu("http://www.newegg.com/Product/Product.aspx?Item=N82E16814487159");
+    	RamPart ram;
+    	ram = getRam("http://www.newegg.com/Product/Product.aspx?Item=N82E16820233144");
     }
     
     //public Search(PartType type)
@@ -261,6 +265,280 @@ public class Search{
 
         //return the CPUPART
     	return new MotherboardPart(brand, model, socketType);
+    }
+    
+    public static GpuPart getGpu(String url) throws IOException
+    {
+    	//open the webpage
+        //String url = "http://www.newegg.com/Product/Product.aspx?Item=N82E16819117369";
+        Document document = Jsoup.connect(url).get();
+        
+        String html = document.toString();
+        
+        String brand= new String();
+        String model = new String();
+        String slotType = new String();
+        String gpuBase = new String();
+        String memorySize = new String();
+        
+        int startSub = 0;
+        int endSub = 0;
+        
+        //Find where the specs are and retrieve
+        for(int i = 0; i < html.length(); i++)
+        {
+        	if(i == 10000)
+        	{
+        		html = html.substring(i, html.length());
+        		i = 0;             
+            } 
+        	if(html.substring(i, i+5).equals("Specs"))
+        	{
+        		String id = html.substring(i - 4, i-2);
+        		if(id.equals("id"))
+        		{
+        			for(int j = i; j < i + 5000; j++)
+        			{
+        				if(html.substring(j, j + 5).equals("Brand"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					brand = html.substring(startSub , endSub);
+        				}
+        				
+        				if(html.substring(j, j + 5).equals("Model"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					model = html.substring(startSub , endSub);
+        				}
+        				
+        				if(html.substring(j, j + 10).equals(">Interface"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					slotType = html.substring(startSub , endSub);
+        				}
+        				
+        				if(html.substring(j, j + 11).equals("Memory Size"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					memorySize = html.substring(startSub , endSub);
+        				}
+        				
+        				if(html.substring(j, j + 11).equals("Memory Type"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					gpuBase = html.substring(startSub , endSub);
+        				}
+        				
+        			}
+        			break;
+        		}
+        	}
+        	
+        }
+
+        //return the CPUPART
+    	return new GpuPart(brand, model, slotType, gpuBase, memorySize);
+    }
+    
+    public static RamPart getRam(String url) throws IOException
+    {
+    	//open the webpage
+        //String url = "http://www.newegg.com/Product/Product.aspx?Item=N82E16819117369";
+        Document document = Jsoup.connect(url).get();
+        
+        String html = document.toString();
+        
+        String brand= new String();
+        String model = new String();
+        String series = new String();
+        String capacity = new String();
+        String type = new String();
+        String multichannelType = new String();
+        
+        int startSub = 0;
+        int endSub = 0;
+        
+        //Find where the specs are and retrieve
+        for(int i = 0; i < html.length(); i++)
+        {
+        	if(i == 10000)
+        	{
+        		html = html.substring(i, html.length());
+        		i = 0;             
+            } 
+        	if(html.substring(i, i+5).equals("Specs"))
+        	{
+        		String id = html.substring(i - 4, i-2);
+        		if(id.equals("id"))
+        		{
+        			for(int j = i; j < i + 5000; j++)
+        			{
+        				if(html.substring(j, j + 5).equals("Brand"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					brand = html.substring(startSub , endSub);
+        				}
+        				
+        				if(html.substring(j, j + 5).equals("Model"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					model = html.substring(startSub , endSub);
+        				}
+        				
+        				if(html.substring(j, j + 6).equals("Series"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					series = html.substring(startSub , endSub);
+        				}
+        				
+        				if(html.substring(j, j + 8).equals("Capacity"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					capacity = html.substring(startSub , endSub);
+        				}
+        				
+        				if(html.substring(j, j + 4).equals("Type"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					type = html.substring(startSub , endSub);
+        				}
+        				
+        				if(html.substring(j, j + 17).equals("Multi-channel Kit"))
+        				{
+        					for(int k = j; k < j + 200; k++)
+        					{
+        						if(html.substring(k, k + 4).equals("<dd>"))
+        						{
+        							startSub = k + 20;
+        						}
+        						if(html.substring(k, k + 5).equals("</dd>"))
+        						{
+        							endSub = k - 15;
+        							break;
+        						}
+        					}
+        					multichannelType = html.substring(startSub , endSub);
+        				}
+        				
+        			}
+        			break;
+        		}
+        	}
+        	
+        }
+
+        //return the CPUPART
+    	return new RamPart(brand, series, model, capacity, type, multichannelType);
     }
 
 }
